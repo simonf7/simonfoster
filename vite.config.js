0bafffd9fa31228ec05ca5743e0f97ab0c6bfe3b
Simon Foster <simonf7@hotmail.com>
2026-04-08 23:05:32 +0100

    fix(deploy): Update Vite base configuration for GitHub pages

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  base: './',
+  base: '/simonfoster/',
   plugins: [
     tailwindcss(),
   ],
